1ecb7bd011c1c59c7c5f67f312d6a1ac4e123f8e
Ignacio Esteves <149106695+NachitoE@users.noreply.github.com>
2025-07-13 22:56:00 -0300

    new form + receiving images url instead of relative path in folders

--- src/api.ts
+++ src/api.ts
@@ -1,17 +1,29 @@
-import type { Bird } from "./models/bird";
+import type { Bird } from "shared-types";
 
 export const API_URL = "http://localhost:3000";
 export const API_GET_ALL_BIRDS_URL = `${API_URL}/api/birds`;
 export const API_GET_BIRD_URL = `${API_URL}/api/birds`;
 
 export async function getAllBirds(): Promise<Bird[]> {
-  const res = await fetch(API_GET_ALL_BIRDS_URL);
-  const json = await res.json();
-  return json.data;
+	const res = await fetch(API_GET_ALL_BIRDS_URL);
+	const json = await res.json();
+	return json.data;
 }
 
 export async function getBirdById(id: string): Promise<Bird> {
-  const res = await fetch(`${API_GET_BIRD_URL}/${id}`);
-  const json = await res.json();
-  return json.data;
+	const res = await fetch(`${API_GET_BIRD_URL}/${id}`);
+	const json = await res.json();
+	return json.data;
+}
+
+export async function createBird(bird: Omit<Bird, "id">): Promise<Bird> {
+	const res = await fetch(API_GET_ALL_BIRDS_URL, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(bird),
+	});
+	const json = await res.json();
+	return json.data;
 }
--- src/components/BirdCard.tsx
+++ src/components/BirdCard.tsx
@@ -1,34 +1,34 @@
 import type { Bird } from "shared-types";
 
 interface BirdCardProps {
-  bird: Bird;
-  onClickEvent: () => void;
+	bird: Bird;
+	onClickEvent: () => void;
 }
 
 export default function BirdCard({ bird, onClickEvent }: BirdCardProps) {
-  return (
-    <div
-      className="group bg-white dark:bg-blue-950 shadow-lg rounded-xl overflow-hidden w-full max-w-sm cursor-pointer hover:shadow-2xl transition-shadow"
-      onClick={onClickEvent}
-    >
-      <div className="relative w-100 mx-auto h-48 overflow-hidden">
-        <img
-          className="absolute inset-0 w-full h-full object-cover object-top group-hover:scale-110 transition-transform duration-300"
-          src={`/images/${bird.imageURL}`}
-          alt={bird.scientificName}
-        />
-      </div>
-      <div className="p-4">
-        <h2 className="text-xl font-bold text-gray-900 dark:text-white">
-          {bird.name}
-        </h2>
-        <h4 className="italic text-sm text-gray-600 dark:text-gray-300 underline">
-          {bird.scientificName}
-        </h4>
-        <p className="mt-2 text-gray-700 dark:text-gray-200 line-clamp-3">
-          {bird.description}
-        </p>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			className="group bg-white dark:bg-blue-950 shadow-lg rounded-xl overflow-hidden w-full max-w-sm cursor-pointer hover:shadow-2xl transition-shadow"
+			onClick={onClickEvent}
+		>
+			<div className="relative w-100 mx-auto h-48 overflow-hidden">
+				<img
+					className="absolute inset-0 w-full h-full object-cover object-top group-hover:scale-110 transition-transform duration-300"
+					src={`${bird.imageURL}`}
+					alt={bird.scientificName}
+				/>
+			</div>
+			<div className="p-4">
+				<h2 className="text-xl font-bold text-gray-900 dark:text-white">
+					{bird.name}
+				</h2>
+				<h4 className="italic text-sm text-gray-600 dark:text-gray-300 underline">
+					{bird.scientificName}
+				</h4>
+				<p className="mt-2 text-gray-700 dark:text-gray-200 line-clamp-3">
+					{bird.description}
+				</p>
+			</div>
+		</div>
+	);
 }
