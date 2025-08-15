1e8ce8590a86b42d219a469bb7f5b1c59fa27453
Ignacio Esteves <149106695+NachitoE@users.noreply.github.com>
2025-08-15 17:59:34 -0300

    ordering comments by date

--- src/components/BirdCommentList.tsx
+++ src/components/BirdCommentList.tsx
@@ -5,68 +5,74 @@ import type { Comment, User } from "shared-types";
 type CommentWithUser = Comment & { user: User };
 
 type BirdCommentListProps = {
-  birdId: string;
-  refreshKey?: number;
+	birdId: string;
+	refreshKey?: number;
 };
 export default function BirdCommentList({
-  birdId,
-  refreshKey,
+	birdId,
+	refreshKey,
 }: BirdCommentListProps) {
-  const [birdComments, setBirdComments] = useState<CommentWithUser[]>([]);
+	const [birdComments, setBirdComments] = useState<CommentWithUser[]>([]);
 
-  useEffect(() => {
-    async function fetchCommentsWithAvatars() {
-      const comments = await getBirdCommentsById(birdId);
-      const commentsWithUsers = await Promise.all(
-        comments.map(async (comment) => {
-          const user = await getUserById(comment.userId);
-          return {
-            ...comment,
-            user: user!,
-          };
-        })
-      );
-      setBirdComments(commentsWithUsers);
-    }
-    fetchCommentsWithAvatars();
-  }, [birdId, refreshKey]);
+	useEffect(() => {
+		async function fetchCommentsWithAvatars() {
+			const comments = await getBirdCommentsById(birdId);
+			//order by date
+			comments.sort((a, b) => {
+				return (
+					new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+				);
+			});
+			const commentsWithUsers = await Promise.all(
+				comments.map(async (comment) => {
+					const user = await getUserById(comment.userId);
+					return {
+						...comment,
+						user: user!,
+					};
+				})
+			);
+			setBirdComments(commentsWithUsers);
+		}
+		fetchCommentsWithAvatars();
+	}, [birdId, refreshKey]);
 
-  if (!birdComments) {
-    return (
-      <div className="text-center text-lg text-lime-700">
-        Cargando comentarios...
-      </div>
-    );
-  }
-  if (birdComments.length === 0) {
-    return (
-      <div>
-        <h2 className="text-2xl font-bold text-lime-800 mb-4">Comentarios</h2>
-        <p>No hay comentarios para este pájaro.</p>
-      </div>
-    );
-  }
-  return (
-    <div className="border-2 rounded-md p-4 w-full">
-      <h2 className="text-2xl font-bold text-lime-800 mb-4">Comentarios</h2>
-      {birdComments.map((element) => (
-        <div className="border-b border-gray-200 py-2" key={element.id}>
-          <p className="text-gray-700">
-            {/* TODO: Poner avatar default */}
-            {element.user.avatarURL && (
-              <img
-                src={element.user.avatarURL}
-                alt={`${element.userId}'s avatar`}
-                className="inline-block w-8 h-8 rounded-full mr-2"
-              />
-            )}
-            <strong>{element.user.name}</strong>: {element.text}
-          </p>
-          <p className="text-sm text-gray-500">
-            {new Date(element.createdAt).toLocaleDateString()}
-          </p>
-        </div>
-      ))}
-    </div>
-  );
+	if (!birdComments) {
+		return (
+			<div className="text-center text-lg text-lime-700">
+				Cargando comentarios...
+			</div>
+		);
+	}
+	if (birdComments.length === 0) {
+		return (
+			<div>
+				<h2 className="text-2xl font-bold text-lime-800 mb-4">Comentarios</h2>
+				<p>No hay comentarios para este pájaro.</p>
+			</div>
+		);
+	}
+	return (
+		<div className="border-2 rounded-md p-4 w-full">
+			<h2 className="text-2xl font-bold text-lime-800 mb-4">Comentarios</h2>
+			{birdComments.map((element) => (
+				<div className="border-b border-gray-200 py-2" key={element.id}>
+					<p className="text-gray-700">
+						{/* TODO: Poner avatar default */}
+						{element.user.avatarURL && (
+							<img
+								src={element.user.avatarURL}
+								alt={`${element.userId}'s avatar`}
+								className="inline-block w-8 h-8 rounded-full mr-2"
+							/>
+						)}
+						<strong>{element.user.name}</strong>: {element.text}
+					</p>
+					<p className="text-sm text-gray-500">
+						{new Date(element.createdAt).toLocaleDateString()}
+					</p>
+				</div>
+			))}
+		</div>
+	);
 }
--- src/components/CreateBirdForm.tsx
+++ src/components/CreateBirdForm.tsx
@@ -2,193 +2,191 @@ import { useState } from "react";
 import type { Bird } from "shared-types";
 import { createBird } from "../api";
 import { useAuth } from "../context/AuthContext";
+import axios from "axios";
 
 export default function CreateBirdForm() {
-  // -- VALIDATION --
-  const user = useAuth().user;
-  console.log(user);
-  if (!user) {
-    return (
-      <div className="flex items-center h-screen justify-center">
-        <p className="text-red-500">
-          Debes iniciar sesión y ser un administrador para crear un ave.
-        </p>
-      </div>
-    );
-  }
-  if (!user.role || user.role !== "admin") {
-    return (
-      <div className="flex items-center h-screen justify-center">
-        <p className="text-red-500">
-          Solo los administradores pueden crear aves.
-        </p>
-      </div>
-    );
-  }
+	// -- VALIDATION --
+	const user = useAuth().user;
+	console.log(user);
+	if (!user) {
+		return (
+			<div className="flex items-center h-screen justify-center">
+				<p className="text-red-500">
+					Debes iniciar sesión y ser un administrador para crear un ave.
+				</p>
+			</div>
+		);
+	}
+	if (!user.role || user.role !== "admin") {
+		return (
+			<div className="flex items-center h-screen justify-center">
+				<p className="text-red-500">
+					Solo los administradores pueden crear aves.
+				</p>
+			</div>
+		);
+	}
 
-  // -- FORM STATE --
-  const [formData, setFormData] = useState({
-    name: "",
-    scientificName: "",
-    description: "",
-    image: null as File | null,
-  });
+	// -- FORM STATE --
+	const [formData, setFormData] = useState({
+		name: "",
+		scientificName: "",
+		description: "",
+		image: null as File | null,
+	});
 
-  const [previewURL, setPreviewURL] = useState<string | null>(null);
-  const [uploading, setUploading] = useState(false);
-  const [result, setResult] = useState<string | null>(null);
+	const [previewURL, setPreviewURL] = useState<string | null>(null);
+	const [uploading, setUploading] = useState(false);
+	const [result, setResult] = useState<string | null>(null);
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value, type, files } = e.target;
+	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, value, type, files } = e.target;
 
-    if (type === "file" && files && files[0]) {
-      const file = files[0];
-      setFormData((prev) => ({ ...prev, image: file }));
-      setPreviewURL(URL.createObjectURL(file));
-    } else {
-      setFormData((prev) => ({ ...prev, [name]: value }));
-    }
-  };
+		if (type === "file" && files && files[0]) {
+			const file = files[0];
+			setFormData((prev) => ({ ...prev, image: file }));
+			setPreviewURL(URL.createObjectURL(file));
+		} else {
+			setFormData((prev) => ({ ...prev, [name]: value }));
+		}
+	};
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    if (!formData.name || !formData.scientificName || !formData.description) {
-      setResult("Por favor, completa todos los campos requeridos.");
-      return;
-    }
-    setUploading(true);
-    let imageURL = "";
+	const handleSubmit = async (e: React.FormEvent) => {
+		e.preventDefault();
+		if (!formData.name || !formData.scientificName || !formData.description) {
+			setResult("Por favor, completa todos los campos requeridos.");
+			return;
+		}
+		setUploading(true);
+		let imageURL = "";
 
-    if (formData.image) {
-      const uploadData = new FormData();
-      uploadData.append("file", formData.image);
-      uploadData.append("upload_preset", "avistandoo");
-      uploadData.append("folder", "aves");
+		if (formData.image) {
+			const uploadData = new FormData();
+			uploadData.append("file", formData.image);
+			uploadData.append("upload_preset", "avistandoo");
+			uploadData.append("folder", "aves");
 
-      try {
-        const imageResponse = await fetch(
-          "https://api.cloudinary.com/v1_1/dzxlynhfm/image/upload",
-          {
-            method: "POST",
-            body: uploadData,
-          }
-        );
+			try {
+				const imageResponse = await axios.post(
+					"https://api.cloudinary.com/v1_1/dzxlynhfm/image/upload",
+					uploadData
+				);
 
-        const imageUploadData = await imageResponse.json();
-        imageURL = imageUploadData.secure_url;
-      } catch (error) {
-        console.error("Error al subir imagen:", error);
-        setResult("Error al subir la imagen. Por favor, inténtalo de nuevo.");
-        setUploading(false);
-        return;
-      }
-    }
+				const imageUploadData = await imageResponse.data;
+				imageURL = imageUploadData.secure_url;
+			} catch (error) {
+				console.error("Error al subir imagen:", error);
+				setResult("Error al subir la imagen. Por favor, inténtalo de nuevo.");
+				setUploading(false);
+				return;
+			}
+		}
 
-    const newBird: Omit<Bird, "id"> = {
-      name: formData.name,
-      scientificName: formData.scientificName,
-      description: formData.description,
-      imageURL,
-    };
-    //await for finished createBird??
-    const response = await createBird(newBird, user);
-    if (response.status !== 201) {
-      setUploading(false);
-      setResult(response.message || "Error al crear el pájaro.");
-      return;
-    }
-    setUploading(false);
-    setResult("Pájaro creado correctamente. ");
-  };
+		const newBird: Omit<Bird, "id"> = {
+			name: formData.name,
+			scientificName: formData.scientificName,
+			description: formData.description,
+			imageURL,
+		};
+		//await for finished createBird??
+		const response = await createBird(newBird, user);
+		if (response.status !== 201) {
+			setUploading(false);
+			setResult(response.message || "Error al crear el pájaro.");
+			return;
+		}
+		setUploading(false);
+		setResult("Pájaro creado correctamente. ");
+	};
 
-  return (
-    <div className="max-w-xl mx-auto">
-      <h1 className="text-center text-3xl font-bold mb-6">
-        🐦 AGREGAR NUEVO AVE 🐦
-      </h1>
-      <form onSubmit={handleSubmit} className="space-y-4">
-        {createInput(
-          "text",
-          "name",
-          "Cotorra Argentina",
-          "Nombre del Ave",
-          formData.name,
-          handleInputChange,
-          true
-        )}
-        {createInput(
-          "text",
-          "scientificName",
-          "Myiopsitta monachus",
-          "Nombre Científico",
-          formData.scientificName,
-          handleInputChange,
-          true
-        )}
-        {createInput(
-          "file",
-          "image",
-          "",
-          "Imagen del Ave",
-          "",
-          handleInputChange
-        )}
-        {createInput(
-          "text",
-          "description",
-          "Es un ave que...",
-          "Descripción",
-          formData.description,
-          handleInputChange,
-          true
-        )}
+	return (
+		<div className="max-w-xl mx-auto">
+			<h1 className="text-center text-3xl font-bold mb-6">
+				🐦 AGREGAR NUEVO AVE 🐦
+			</h1>
+			<form onSubmit={handleSubmit} className="space-y-4">
+				{createInput(
+					"text",
+					"name",
+					"Cotorra Argentina",
+					"Nombre del Ave",
+					formData.name,
+					handleInputChange,
+					true
+				)}
+				{createInput(
+					"text",
+					"scientificName",
+					"Myiopsitta monachus",
+					"Nombre Científico",
+					formData.scientificName,
+					handleInputChange,
+					true
+				)}
+				{createInput(
+					"file",
+					"image",
+					"",
+					"Imagen del Ave",
+					"",
+					handleInputChange
+				)}
+				{createInput(
+					"text",
+					"description",
+					"Es un ave que...",
+					"Descripción",
+					formData.description,
+					handleInputChange,
+					true
+				)}
 
-        <button
-          type="submit"
-          disabled={uploading}
-          className={`w-full px-4 py-2 rounded ${
-            uploading ? "bg-gray-500" : "bg-blue-600 hover:bg-blue-700"
-          } text-white`}
-        >
-          {uploading ? "Subiendo..." : "Guardar Ave"}
-        </button>
-      </form>
+				<button
+					type="submit"
+					disabled={uploading}
+					className={`w-full px-4 py-2 rounded ${
+						uploading ? "bg-gray-500" : "bg-blue-600 hover:bg-blue-700"
+					} text-white`}
+				>
+					{uploading ? "Subiendo..." : "Guardar Ave"}
+				</button>
+			</form>
 
-      {previewURL && (
-        <div className="mt-4 text-center">
-          <p className="font-semibold">Vista previa de la imagen:</p>
-          <img src={previewURL} alt="Preview" className="mx-auto max-h-64" />
-        </div>
-      )}
+			{previewURL && (
+				<div className="mt-4 text-center">
+					<p className="font-semibold">Vista previa de la imagen:</p>
+					<img src={previewURL} alt="Preview" className="mx-auto max-h-64" />
+				</div>
+			)}
 
-      {result && <p className="text-green-600 text-center mt-4">{result}</p>}
-    </div>
-  );
+			{result && <p className="text-green-600 text-center mt-4">{result}</p>}
+		</div>
+	);
 }
 
 function createInput(
-  type: string,
-  name: string,
-  placeholder: string,
-  label: string,
-  value: string,
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
-  required: boolean = false
+	type: string,
+	name: string,
+	placeholder: string,
+	label: string,
+	value: string,
+	onChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
+	required: boolean = false
 ) {
-  return (
-    <div>
-      <label className="block text-lg font-medium text-gray-700 text-center">
-        {required && <span className="text-red-500">*</span>}
-        {label}
-      </label>
-      <input
-        type={type}
-        name={name}
-        placeholder={placeholder}
-        value={type === "file" ? undefined : value}
-        onChange={onChange}
-        className="border px-3 py-2 w-full rounded text-center"
-      />
-    </div>
-  );
+	return (
+		<div>
+			<label className="block text-lg font-medium text-gray-700 text-center">
+				{required && <span className="text-red-500">*</span>}
+				{label}
+			</label>
+			<input
+				type={type}
+				name={name}
+				placeholder={placeholder}
+				value={type === "file" ? undefined : value}
+				onChange={onChange}
+				className="border px-3 py-2 w-full rounded text-center"
+			/>
+		</div>
+	);
 }
